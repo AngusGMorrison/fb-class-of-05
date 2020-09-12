dd22feceb4a76e4358961546aa6ec0f84c09a1b9
Angus Morrison <contact@angus-morrison.com>
2020-09-12 17:17:56 +0100

    Temporarily disable migrate and clean gomod

--- cmd/migrate/migrate_test.go
+++ cmd/migrate/migrate_test.go
@@ -1,121 +1,121 @@
 package main
 
-import (
-	"angusgmorrison/fb05/pkg/envloader"
-	"bytes"
-	"errors"
-	"fmt"
-	"testing"
-)
+// import (
+// 	"angusgmorrison/fb05/pkg/envloader"
+// 	"bytes"
+// 	"errors"
+// 	"fmt"
+// 	"testing"
+// )
 
-type mockMigrator struct {
-	lastFuncCalled string
-}
+// type mockMigrator struct {
+// 	lastFuncCalled string
+// }
 
-func (m *mockMigrator) Down() error {
-	m.lastFuncCalled = "Down"
-	return nil
-}
+// func (m *mockMigrator) Down() error {
+// 	m.lastFuncCalled = "Down"
+// 	return nil
+// }
 
-func (m *mockMigrator) Drop() error {
-	m.lastFuncCalled = "Drop"
-	return nil
-}
+// func (m *mockMigrator) Drop() error {
+// 	m.lastFuncCalled = "Drop"
+// 	return nil
+// }
 
-func (m *mockMigrator) Migrate(_ uint) error {
-	m.lastFuncCalled = "Migrate"
-	return nil
-}
+// func (m *mockMigrator) Migrate(_ uint) error {
+// 	m.lastFuncCalled = "Migrate"
+// 	return nil
+// }
 
-func (m *mockMigrator) Force(_ int) error {
-	m.lastFuncCalled = "Force"
-	return nil
-}
+// func (m *mockMigrator) Force(_ int) error {
+// 	m.lastFuncCalled = "Force"
+// 	return nil
+// }
 
-func (m *mockMigrator) Steps(_ int) error {
-	m.lastFuncCalled = "Steps"
-	return nil
-}
+// func (m *mockMigrator) Steps(_ int) error {
+// 	m.lastFuncCalled = "Steps"
+// 	return nil
+// }
 
-func (m *mockMigrator) Up() error {
-	m.lastFuncCalled = "Up"
-	return nil
-}
+// func (m *mockMigrator) Up() error {
+// 	m.lastFuncCalled = "Up"
+// 	return nil
+// }
 
-func (m *mockMigrator) Version() (uint, bool, error) {
-	m.lastFuncCalled = "Version"
-	return 1, false, nil
-}
+// func (m *mockMigrator) Version() (uint, bool, error) {
+// 	m.lastFuncCalled = "Version"
+// 	return 1, false, nil
+// }
 
-func TestRunMigration(t *testing.T) {
-	testCases := []struct {
-		command        string
-		args           []string
-		wantLastCalled string
-		wantErr        bool
-		wantOutput     string
-	}{
-		{"down", nil, "Down", false, ""},
-		{"drop", nil, "Drop", false, ""},
-		{"force", []string{"1"}, "Force", false, ""},
-		{"force", []string{"@"}, "", true, ""},
-		{"steps", []string{"1"}, "Steps", false, ""},
-		{"toVersion", []string{"1"}, "Migrate", false, ""},
-		{"up", nil, "Up", false, ""},
-		{"version", nil, "Version", false, "\tVersion: 1\n\tDirty: false\n"},
-		{"unknown", nil, "", true, ""},
-	}
+// func TestRunMigration(t *testing.T) {
+// 	testCases := []struct {
+// 		command        string
+// 		args           []string
+// 		wantLastCalled string
+// 		wantErr        bool
+// 		wantOutput     string
+// 	}{
+// 		{"down", nil, "Down", false, ""},
+// 		{"drop", nil, "Drop", false, ""},
+// 		{"force", []string{"1"}, "Force", false, ""},
+// 		{"force", []string{"@"}, "", true, ""},
+// 		{"steps", []string{"1"}, "Steps", false, ""},
+// 		{"toVersion", []string{"1"}, "Migrate", false, ""},
+// 		{"up", nil, "Up", false, ""},
+// 		{"version", nil, "Version", false, "\tVersion: 1\n\tDirty: false\n"},
+// 		{"unknown", nil, "", true, ""},
+// 	}
 
-	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("runMigration(m, %q, %v)", tc.command, tc.args), func(t *testing.T) {
-			if tc.wantOutput != "" {
-				oldOut := out
-				out = new(bytes.Buffer)
-				defer func() {
-					out = oldOut
-				}()
-			}
+// 	for _, tc := range testCases {
+// 		t.Run(fmt.Sprintf("runMigration(m, %q, %v)", tc.command, tc.args), func(t *testing.T) {
+// 			if tc.wantOutput != "" {
+// 				oldOut := out
+// 				out = new(bytes.Buffer)
+// 				defer func() {
+// 					out = oldOut
+// 				}()
+// 			}
 
-			var m mockMigrator
-			err := runMigration(&m, tc.command, tc.args)
-			if gotErr := err != nil; gotErr != tc.wantErr {
-				t.Fatalf("wantErr == %t, but got %v", tc.wantErr, err)
-			}
-			if m.lastFuncCalled != tc.wantLastCalled {
-				t.Fatalf("want lastFuncCalled to be %q, got %q",
-					tc.wantLastCalled, m.lastFuncCalled)
-			}
-			if tc.wantOutput != "" {
-				if gotOutput := out.(*bytes.Buffer).String(); gotOutput != tc.wantOutput {
-					t.Errorf("want output %q, got %q", tc.wantOutput, gotOutput)
-				}
-			}
-		})
+// 			var m mockMigrator
+// 			err := runMigration(&m, tc.command, tc.args)
+// 			if gotErr := err != nil; gotErr != tc.wantErr {
+// 				t.Fatalf("wantErr == %t, but got %v", tc.wantErr, err)
+// 			}
+// 			if m.lastFuncCalled != tc.wantLastCalled {
+// 				t.Fatalf("want lastFuncCalled to be %q, got %q",
+// 					tc.wantLastCalled, m.lastFuncCalled)
+// 			}
+// 			if tc.wantOutput != "" {
+// 				if gotOutput := out.(*bytes.Buffer).String(); gotOutput != tc.wantOutput {
+// 					t.Errorf("want output %q, got %q", tc.wantOutput, gotOutput)
+// 				}
+// 			}
+// 		})
 
-	}
-}
+// 	}
+// }
 
-func TestMigrationError(t *testing.T) {
-	command := "test"
-	err := errors.New("error message")
-	wantErrorMsg := fmt.Sprintf("migrate %s: %v", command, err)
-	gotErrorMsg := migrationError(command, err).Error()
-	if gotErrorMsg != wantErrorMsg {
-		t.Errorf("want error message %q, got %q", wantErrorMsg, gotErrorMsg)
-	}
-}
+// func TestMigrationError(t *testing.T) {
+// 	command := "test"
+// 	err := errors.New("error message")
+// 	wantErrorMsg := fmt.Sprintf("migrate %s: %v", command, err)
+// 	gotErrorMsg := migrationError(command, err).Error()
+// 	if gotErrorMsg != wantErrorMsg {
+// 		t.Errorf("want error message %q, got %q", wantErrorMsg, gotErrorMsg)
+// 	}
+// }
 
-func TestDatabaseURL(t *testing.T) {
-	envConfig := envloader.NewConfig("environment", "yaml", "fixtures", "test")
-	var err error
-	envVars, err = envloader.Load(envConfig)
-	if err != nil {
-		t.Fatalf("failed to load environment: %v", err)
-	}
-	defer func() { envVars = nil }()
+// func TestDatabaseURL(t *testing.T) {
+// 	envConfig := envloader.NewConfig("environment", "yaml", "fixtures", "test")
+// 	var err error
+// 	envVars, err = envloader.Load(envConfig)
+// 	if err != nil {
+// 		t.Fatalf("failed to load environment: %v", err)
+// 	}
+// 	defer func() { envVars = nil }()
 
-	wantDBURL := "postgres://test_user:password@test_host:1234/test_db?sslmode=disable"
-	if gotDBURL := databaseURL(); gotDBURL != wantDBURL {
-		t.Errorf("want DB URL %q, got %q", wantDBURL, gotDBURL)
-	}
-}
+// 	wantDBURL := "postgres://test_user:password@test_host:1234/test_db?sslmode=disable"
+// 	if gotDBURL := databaseURL(); gotDBURL != wantDBURL {
+// 		t.Errorf("want DB URL %q, got %q", wantDBURL, gotDBURL)
+// 	}
+// }
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module angusgmorrison/fb05
 go 1.15
 
 require (
-	github.com/golang-migrate/migrate/v4 v4.12.2
 	github.com/gorilla/mux v1.8.0
+	github.com/kylelemons/godebug v1.1.0
 	github.com/pkg/errors v0.9.1
-	github.com/rs/zerolog v1.19.0
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.5.1 // indirect
+	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
+	golang.org/x/text v0.3.3 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
